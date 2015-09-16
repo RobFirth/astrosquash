37334de5427a400f0d2f0768157185f11bb8ffe9
Rob Firth <ref1g12@soton.ac.uk>
2015-09-16 15:13:39 +0100

    laying db groundwork

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
- 
+from flask.ext.sqlalchemy import SQLAlchemy
+from .models import Player
+
 app = Flask(__name__)      
- 
+db = SQLAlchemy(app)
+
 @app.route('/')
 def home():
   return render_template('home.html')
+
+from app import models
  
 if __name__ == '__main__':
   app.run(debug=True)
