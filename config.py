f8011d9a40450a5c1c7990bb8d70823fdc577a14
Rob Firth <ref1g12@soton.ac.uk>
2015-09-16 17:29:08 +0100

    reverted

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ SECRET_KEY = 'you-will-never-guess'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///home/berto54/astrosquash/app.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///home/berto54/astrosquash/app.db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
