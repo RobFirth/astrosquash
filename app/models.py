b0100c45131fa6f7d327029ecee053467fe616d3
Rob Firth <r.firth@soton.ac.uk>
2015-10-04 10:30:33 +0100

    Trying to figure out fixtures

--- app/models.py
+++ app/models.py
@@ -1,14 +1,5 @@
 from app import db
 
-class Fixtures(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    timestamp = db.Column(db.DateTime)
-    
-    winner_id = db.Column(db.Integer, index = True)
-    loser_id = db.Column(db.Integer, index = True)
-    def __repr__(self):
-        return '<Fixture %r>' % (self.timestamp)
-
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     played = db.Column(db.Integer, index=True)
@@ -16,6 +7,13 @@ class Player(db.Model):
     lost = db.Column(db.Integer, index=True)
     name = db.Column(db.String(64), index=True)
     photourl = db.Column(db.String(120), index=True, unique=True)
-    
     def __repr__(self):
             return '<User %r>' % (self.name)
+
+class Fixtures(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    timestamp = db.Column(db.DateTime)
+    winner_id = db.Column(db.Integer, db.ForeignKey('Player.id'))
+    loser_id = db.Column(db.Integer, db.ForeignKey('Player.id'))
+    def __repr__(self):
+        return '<Fixture %r>' % (self.timestamp)
